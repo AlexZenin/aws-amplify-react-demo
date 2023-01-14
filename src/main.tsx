2eaf905f68f3e105edeb02bd9f63ed02f74589b9
Alex Zenin <alex@alexzenin.com>
2023-01-14 18:50:17 +1100

    build: configure amplify with vite so that the project successfully builds

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import { Amplify } from 'aws-amplify'
+import config from './aws-exports.js'
+Amplify.configure(config)
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+        // So that aws amplify plays nice with Vite
+        "./runtimeConfig": "./runtimeConfig.browser",
+    }
+  }
 })
